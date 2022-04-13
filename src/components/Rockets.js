5677c80f1d92663b87087fc33def7cd3e8a28469
alef-garrido <oa.g11@icloud.com>
2022-04-13 10:35:32 -0500

    Refactor middleware property to get default middleware working

--- src/components/Rockets.js
+++ src/components/Rockets.js
@@ -1,19 +1,31 @@
-import { useSelector } from 'react-redux';
+import { useDispatch, useSelector } from 'react-redux';
+import { rocketReserved } from '../Redux/slices/rockets-dux';
 
 function Rockets() {
+  const dispatch = useDispatch();
   const rockets = useSelector((state) => state.rockets);
   const { list, isLoading } = rockets;
+
+  const clickHandler = (e) => {
+    dispatch(rocketReserved(e.target.parentNode.id));
+  };
+
   return (
     <ul>
       {
       isLoading
         ? 'loading'
         : list.map((item) => (
-          <li key={item.id}>
+          <li key={item.id} id={item.id}>
             <img alt="" src={item.flickr_images[0]} />
             <p>{item.rocket_name}</p>
             <p>{item.rocket_type}</p>
-            <button type="button">Reserve Rocket</button>
+            <button
+              type="button"
+              onClick={clickHandler}
+            >
+              Reserve Rocket
+            </button>
           </li>
         ))
       }
