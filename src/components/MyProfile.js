94e944c0f5154da26f278ef581545018fa08d426
alef-garrido <oa.g11@icloud.com>
2022-04-13 16:12:18 -0500

    Refactor MyProfile component to display reserved Rockets list

--- src/components/MyProfile.js
+++ src/components/MyProfile.js
@@ -1,8 +1,21 @@
 import React from 'react';
+import { useSelector } from 'react-redux';
 
 function MyProfile() {
+  const rockets = useSelector((state) => state.rockets.list);
   return (
-    <h3>Profile</h3>
+    <div>
+      <section>
+        <h3>My Rockets</h3>
+        <ul className="reserved--list">
+          {
+            rockets
+              ? rockets.map((rocket) => <li key={rocket.id}>{rocket.rocket_name}</li>)
+              : null
+          }
+        </ul>
+      </section>
+    </div>
   );
 }
 
