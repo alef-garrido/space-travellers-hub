6ee9400c4a387ef6b3c9c259347b977295fed6a0
alef-garrido <oa.g11@icloud.com>
2022-04-12 21:24:53 -0500

    Refactor Rockets component to comply with assigment

--- src/components/Rockets.js
+++ src/components/Rockets.js
@@ -1,23 +1,6 @@
-import { useSelector } from 'react-redux';
-
 function Rockets() {
-  const rockets = useSelector((state) => state.rockets);
-  const { list, isLoading } = rockets;
   return (
-    <ul>
-      {
-      isLoading
-        ? 'loading'
-        : list.map((item) => (
-          <li key={item.id}>
-            <img alt="" src={item.flickr_images[0]} />
-            <p>{item.rocket_name}</p>
-            <p>{item.rocket_type}</p>
-            <button type="button">Reserve Rocket</button>
-          </li>
-        ))
-      }
-    </ul>
+    <>Loading</>
   );
 }
 
