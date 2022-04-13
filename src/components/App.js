fa9241d93dfad2a07085a09793f46f9a5972d1d5
alef-garrido <oa.g11@icloud.com>
2022-04-12 20:57:07 -0500

    Fixed Routing links

--- src/components/App.js
+++ src/components/App.js
@@ -1,11 +1,16 @@
+import { Routes, Route } from 'react-router-dom';
 import NavLink from './NavLink';
 import Rockets from './Rockets';
+import MyProfile from './MyProfile';
 
 function App() {
   return (
-    <div className="app">
+    <div className="app--container">
       <NavLink />
-      <Rockets />
+      <Routes>
+        <Route path="/" element={<Rockets />} />
+        <Route path="profile" element={<MyProfile />} />
+      </Routes>
     </div>
   );
 }
