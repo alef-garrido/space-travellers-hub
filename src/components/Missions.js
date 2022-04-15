8b59f1d7565c91480427310419fdfd90621edf7d
chukwuebukaVictor <chukwuebuka.victor.ozoede@gmail.com>
2022-04-15 02:31:11 +0100

    Switch badges for missions conditional components

--- src/components/Missions.js
+++ src/components/Missions.js
@@ -1,6 +1,7 @@
 // import { useEffect } from 'react';
 import { useDispatch, useSelector } from 'react-redux';
 import { toggleMissionsReserved } from '../Redux/missions/missions';
+import './Styles/mission.css';
 
 const missionsContainerStyle = {
   hight: '25rem',
@@ -8,6 +9,32 @@ const missionsContainerStyle = {
   justifyContent: 'center',
   margin: '0 2rem',
 };
+
+const joinButtonStyle = {
+  marginLeft: '3rem',
+  cursor: 'pointer',
+};
+
+const leaveButtonStyle = {
+  border: '1px solid rgb(255,0,0)',
+  backgroundColor: 'white',
+  color: 'red',
+  marginLeft: '3rem',
+  cursor: 'pointer',
+};
+
+const membershipStyle = {
+  backgroundColor: '#00ffff',
+  color: '#000000',
+  marginRight: '2rem',
+};
+
+const notMemberStyle = {
+  backgroundColor: '#a9a9a9',
+  color: '#ffffff',
+  marginRight: '2rem',
+};
+
 
 function Missions() {
   const dispatch = useDispatch();
@@ -26,16 +53,11 @@ function Missions() {
             <div key={missions.id} className="d-grid">
               <h3>{missions.name}</h3>
               <p>{missions.descriptions}</p>
-              <button type="button">
-                Leave Mission
-              </button>
-              <button
-                type="button"
-                onClick={() => dispatch(toggleMissionsReserved(missions.id))}
-              >
-                Join mission
-              </button>
-            </div>
+              <div className="d-grid1">
+                <button type="button" style={missions.reserved ? membershipStyle : notMemberStyle}>{missions.reserved ? 'Active member' : 'Not a member'}</button>
+                <button type="button" style={missions.reserved ? leaveButtonStyle : joinButtonStyle} onClick={() => dispatch(toggleMissionsReserved(missions.id))}>{missions.reserved ? 'Leave Mission' : 'Join Mission'}</button>
+              </div>
+              </div>
           ))}
         </div>
       ) : <h1>***Loading***</h1>}
