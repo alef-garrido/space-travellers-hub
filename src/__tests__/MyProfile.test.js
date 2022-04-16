bcd595606fe8c6458e44b61ab64dac472ed90015
chukwuebukaVictor <chukwuebuka.victor.ozoede@gmail.com>
2022-04-16 15:39:54 +0100

    MyProfile test

---
+++ src/__tests__/MyProfile.test.js
@@ -0,0 +1,16 @@
+import React from 'react';
+import { render, screen } from '@testing-library/react';
+import '@testing-library/jest-dom';
+import { Provider } from 'react-redux';
+import MyProfile from '../components/MyProfile';
+import store from '../Redux/store';
+
+it('it works', () => {
+  const tree = render(
+    <Provider store={store}>
+      <MyProfile />
+    </Provider>,
+  );
+  expect(tree).toMatchSnapshot();
+});
+
