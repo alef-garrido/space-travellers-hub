dc44e67c4a11fef4c13cb2d5d7c93332dd170860
alef-garrido <oa.g11@icloud.com>
2022-04-15 23:45:39 -0500

    Fix layout

--- src/components/MyProfile.js
+++ src/components/MyProfile.js
@@ -3,17 +3,22 @@ import { useSelector } from 'react-redux';
 import './Styles/myProfile.css';
 
 function MyProfile() {
-  const rockets = useSelector((state) => state.rockets.list);
-  const reservedList = rockets.filter((rocket) => rocket.reserved === true);
+  const user = useSelector((state) => state);
+  console.log(user);
+  const reservedList = user.rockets.list.filter((rocket) => rocket.reserved === true);
   return (
-    <div>
+    <div className="profile--container">
+      <section className="mission--list-container">
+        <h3>My Missions</h3>
+        <ul className="rocket--list" />
+      </section>
       <section className="rocket--list-container">
         <h3>My Rockets</h3>
         <ul className="rocket--list">
           {
-            rockets
+            reservedList
               ? reservedList.map((rocket) => <li className="rocket-item" key={rocket.id}>{rocket.rocket_name}</li>)
-              : null
+              : 'Reserve your first Rocket'
           }
         </ul>
       </section>
